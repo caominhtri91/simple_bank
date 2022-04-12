aaba68f59f0b5eeba988997776973af1d7b1f483
tri <tri.cao@intrepid.asia>
2022-04-12 15:53:51 +0700

    added environment variable

--- main.go
+++ main.go
@@ -5,18 +5,18 @@ import (
 	"log"
 	"simple-bank/api"
 	db "simple-bank/db/sqlc"
+	"simple-bank/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgres://root:123456@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8888"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,5 +24,5 @@ func main() {
 	store := db.NewStore(conn)
 
 	server := api.NewServer(store)
-	log.Fatal(server.Start(serverAddress))
+	log.Fatal(server.Start(config.ServerAddress))
 }
